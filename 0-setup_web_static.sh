160141d8e29f0bdc91ec7652695fe1c70085825b
Habibafernas <habibafernas@gmail.com>
2024-03-07 15:07:45 -0800

    a

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-#sets up your web servers for the deployment of web_static
+# Sets up a web server for deployment of web_static.
 
 apt-get update
 apt-get install -y nginx
 
-mkdir -P /data/web_static/releases/test/
-mkdir -P /data/web_static/shared/
-echo "Hello World" > /data/web_static/releases/test/index.html
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "Holberton School" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 chown -R ubuntu /data/
